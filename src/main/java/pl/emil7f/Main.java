72a7e07eb12eb3ebdcf94a24c04fff12b0ca8b13
emil7f <emilfigzal@gmail.com>
2021-03-31 20:41:31 +0200

    simple multithread example

--- src/main/java/pl/emil7f/Main.java
+++ src/main/java/pl/emil7f/Main.java
@@ -1,4 +1,30 @@
 package pl.emil7f;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 999; i++) {
+                    System.out.println((Thread.currentThread().getName() + " " + i));
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 999; i++) {
+                    System.out.println((Thread.currentThread().getName() + " " + i));
+                }
+            }
+        });
+        t1.setName("t1");
+        t2.setName("t2");
+
+        t1.start();
+        t2.start();
+    }
 }
