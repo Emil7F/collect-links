676b4a2477abe236653a3ea02d5a229b8610b9ab
emil7f <emilfigzal@gmail.com>
2021-03-31 20:54:40 +0200

    executor

--- src/main/java/pl/emil7f/Main.java
+++ src/main/java/pl/emil7f/Main.java
@@ -1,32 +1,23 @@
 package pl.emil7f;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 999; i++) {
-                    System.out.println((Thread.currentThread().getName() + " " + i));
-                }
-            }
-        });
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        Thread t2 = new Thread(() -> {
-            for (int i = 0; i < 999; i++) {
-                System.out.println((Thread.currentThread().getName() + " " + i));
+        executorService.submit(() -> printNumbers());
+        executorService.submit(() -> printNumbers());
+        executorService.shutdown();
 
-            }
-        });
+    }
 
-        t1.setPriority(1);
-        t2.setPriority(10);
-
-        t1.setName("t1");
-        t2.setName("t2");
-
-        t1.start();
-        t2.start();
+    private static void printNumbers() {
+        for (int i = 0; i < 999; i++) {
+            System.out.println((Thread.currentThread().getName() + " " + i));
+        }
     }
 }
