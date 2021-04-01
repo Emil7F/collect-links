83029645211b13850787b300d910b83e43410c6e
emil7f <emilfigzal@gmail.com>
2021-04-01 20:33:37 +0200

    collecting links from site and add to set

--- src/main/java/pl/emil7f/WebReader.java
+++ src/main/java/pl/emil7f/WebReader.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 public class WebReader {
 
@@ -13,11 +15,28 @@ public class WebReader {
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(url.openStream()));
         String inputLine;
+        StringBuilder builder = new StringBuilder();
         while ((inputLine = in.readLine()) != null) {
-            System.out.println(inputLine);
+            builder.append(inputLine);
+            builder.append(System.lineSeparator());
         }
         in.close();
 
+        Set<String> links = new HashSet<>();
+        String content = builder.toString();
+
+        for (int i = 0; i < content.length(); i++) {
+            i = content.indexOf("https://www.otodom.pl/pl/oferta/", i);
+            if(i < 0){
+                break;
+            }
+            String[] split = content.substring(i).split(".html");
+            String link = split[0];
+            links.add(link);
+
+        }
+
+        links.forEach(string -> System.out.println(string));
 
     }
 }
