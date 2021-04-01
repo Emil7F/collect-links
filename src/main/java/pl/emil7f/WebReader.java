857c072a8398faea5f8c66c73e0b35dbb0c2df24
emil7f <emilfigzal@gmail.com>
2021-04-01 21:07:50 +0200

    read links and save to file

--- src/main/java/pl/emil7f/WebReader.java
+++ src/main/java/pl/emil7f/WebReader.java
@@ -1,8 +1,6 @@
 package pl.emil7f;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
@@ -25,18 +23,28 @@ public class WebReader {
         Set<String> links = new HashSet<>();
         String content = builder.toString();
 
+
         for (int i = 0; i < content.length(); i++) {
             i = content.indexOf("https://www.otodom.pl/pl/oferta/", i);
-            if(i < 0){
+            if (i < 0) {
                 break;
             }
             String[] split = content.substring(i).split(".html");
             String link = split[0];
-            links.add(link);
+            boolean added = links.add(link);
+            try (BufferedWriter bufferedWriter = new BufferedWriter(
+                    new FileWriter("links.txt", true))) {
+                if (added) {
+                    bufferedWriter.write(link + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
         }
 
         links.forEach(string -> System.out.println(string));
+
 
     }
 }
